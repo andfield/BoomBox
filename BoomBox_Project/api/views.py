21c9335d7a57da5b31c62c1703554dfc2995fbb7
siddharththakur76@gmail.com <siddharththakur76@gmail.com>
2021-03-19 04:04:59 +1300

    Join room page done with necessary api

--- BoomBox_Project/api/views.py
+++ BoomBox_Project/api/views.py
@@ -31,11 +31,17 @@ class CreateRoomView(APIView):
                 room.guest_can_pause = guest_can_pause
                 room.votes_to_skip = votes_to_skip
                 room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
+                self.request.session['room_code'] = room.code
+                return Response(RoomSerializer(room).data, status=status.HTTP_200_OK)
+
             else:
                 room = Room(host=host, guest_can_pause=guest_can_pause, votes_to_skip=votes_to_skip)
                 room.save()
-            
-            return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
+                self.request.session['room_code'] = room.code
+                return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
+
+        return Response({'Bad Request': 'Invalid Data'}, status=status.HTTP_400_BAD_REQUEST)
+                
 
 #Find a room with its code.
 class GetRoom(APIView):
@@ -53,3 +59,24 @@ class GetRoom(APIView):
             return Response({'Room Not Found': 'Invalid Room COde.'}, status=status.HTTP_404_NOT_FOUND)
         
         return Response({'Bad Request': 'Broken Request'}, status=status.HTTP_400_BAD_REQUEST)
+
+#class lets user join a certain room
+
+class JoinRoom(APIView):
+    lookup_url_kwarg = 'code'
+
+    def post(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        #get code from frontend
+        code = request.data.get(self.lookup_url_kwarg)
+
+        if code != None:
+            room_result= Room.objects.filter(code=code)
+            if len(room_result) > 0:
+                room = room_result[0]
+                self.request.session['room_code'] = code
+                return Response({'message': 'You have joined the room!'}, status=status.HTTP_200_OK)
+            return Response({'Room Not Found': 'Invalid Room COde.'}, status=status.HTTP_404_NOT_FOUND)
+        return Response({'Bad Request': 'Invalid post data.'}, status=status.HTTP_400_BAD_REQUEST)
--- BoomBox_Project/frontend/src/components/RoomJoiningPage.js
+++ BoomBox_Project/frontend/src/components/RoomJoiningPage.js
@@ -1,10 +1,65 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
+import {TextField, Button, Grid, Typography} from '@material-ui/core'
+import {Link} from 'react-router-dom'
 
-const RoomJoining = () => {
+const RoomJoining=(props) => {
+
+    const [roomCode, changeCode]=useState()
+    const [error, changeError]=useState()
+
+    const buttonClick=() => {
+        console.log(roomCode)
+
+        const requestOptions={
+            method: "POST",
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                code: roomCode
+            })
+        }
+
+        fetch('/api/join-room', requestOptions).
+            then((response) => {
+                if (response.ok) {
+                    console.log(roomCode)
+                    props.history.push('/room/'+roomCode)
+                }
+                else {
+                    changeError({error: 'Room Not Found.'})
+                }
+            }).catch((error) => {
+                console.log(error)
+            })
+    }
+
     return (
-        <h1>
-            This is Room Joining page
-        </h1>
+        <Grid container spacing={1} >
+            <Grid item xs={12} align="center">
+                <Typography variant='h3' component="h3">
+                    Join a Room.
+                </Typography>
+            </Grid>
+            <Grid item xs={12} align="center">
+                <TextField
+                    onChange={e => {changeCode(e.target.value)}}
+                    error={error}
+                    label="Code"
+                    placeholder="Enter a Room Code"
+                    value={roomCode}
+                    helperText={error}
+                    variant="outlined" />
+            </Grid>
+            <Grid item xs={12} align="center">
+                <Button variant="contained" color="primary" onClick={buttonClick}>
+                    Enter Room
+                </Button>
+            </Grid>
+            <Grid item xs={12} align="center">
+                <Button variant="contained" color="secondary" to="/" component={Link}>
+                    Back
+                </Button>
+            </Grid>
+        </Grid>
     )
 }
 export default RoomJoining
