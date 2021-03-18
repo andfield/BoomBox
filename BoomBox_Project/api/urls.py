221c459dd60bc36e2deb85e3a0809c868c427b48
siddharththakur76@gmail.com <siddharththakur76@gmail.com>
2021-03-19 01:20:35 +1300

    Create new room api created

--- BoomBox_Project/api/urls.py
+++ BoomBox_Project/api/urls.py
@@ -1,8 +1,9 @@
 # file to sotre all the urls
 
 from django.urls import path
-from .views import RoomView
+from .views import RoomView, CreateRoomView
 
 urlpatterns = [
-    path('room', RoomView.as_view())
+    path('room', RoomView.as_view()),
+    path('create-room', CreateRoomView.as_view())
 ]
